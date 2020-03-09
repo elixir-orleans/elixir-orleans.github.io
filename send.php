361dde1e92fe8c27711e9e11ba485337e74de037
elixir-orleans <59620082+elixir-orleans@users.noreply.github.com>
2020-03-09 19:04:42 +0100

    Contact

---
+++ send.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un message par formulaire</title>
+</head>
+
+<body>
+    <?php
+      $to      = 'elixir-orleans@gmail.com';
+      $subject = 'Contact - '.$_POST['subject'].' - '.$_POST['name'];
+      $message = $_POST['message'];
+      $headers = array(
+        'From' => $_POST['email'],
+        'Reply-To' => $_POST['email'],
+       'X-Mailer' => 'PHP/' . phpversion()
+      );
+
+    $retour = mail(ùto, $subject, $message, $headers);
+    if ($retour) {
+        echo '<p>Votre message a bien été envoyé.</p>';
+    }
+    ?>
+</body>
+</html>
